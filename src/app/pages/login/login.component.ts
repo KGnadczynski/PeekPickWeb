d2614b3aac87043c9336cd2c61c172d1a535d553
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2016-12-29 23:41:19 +0100

    Logowanie

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {LoginService} from "./loginservice.component";
+import {UserLogin} from "./userlogin";
+import {Router} from "@angular/router";
+import {URLSearchParams} from "@angular/http";
 
 @Component({
   selector: 'login',
   encapsulation: ViewEncapsulation.None,
   styles: [require('./login.scss')],
   template: require('./login.html'),
+  providers: [LoginService]
 })
 export class Login {
 
@@ -13,8 +18,10 @@ export class Login {
   public email:AbstractControl;
   public password:AbstractControl;
   public submitted:boolean = false;
+  userJson: UserLogin;
+  user :any = {};
 
-  constructor(fb:FormBuilder) {
+  constructor(fb:FormBuilder, private loginService: LoginService, private router: Router) {
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
@@ -27,8 +34,24 @@ export class Login {
   public onSubmit(values:Object):void {
     this.submitted = true;
     if (this.form.valid) {
-      // your code goes here
-      // console.log(values);
-    }
+      this.userJson = new UserLogin();
+      this.userJson.email = this.user.email;
+      this.userJson.password = this.user.email.password;
+      this.userJson.client_id = "clientapp";
+      this.userJson.client_secret = "client_secret";
+      this.userJson.grant_type = "password";
+      let body = new URLSearchParams();
+      body.set('username', this.user.email);
+      body.set('password', this.user.email.password);
+      body.set('client_id', "clientapp");
+      body.set('client_secret', "client_secret");
+      body.set('grant_type', "password");
+      this.loginService.login(body).subscribe(
+          data => {
+            this.router.navigate(['/komunikat']);
+          },
+          error => {
+          });
+      }
   }
 }
---
+++ src/app/pages/login/userlogin.ts
@@ -0,0 +1,8 @@
+﻿export class UserLogin {
+    email: string;
+    password: string;
+    grant_type: string;
+    client_secret: string;
+    client_id: string;
+}
+
