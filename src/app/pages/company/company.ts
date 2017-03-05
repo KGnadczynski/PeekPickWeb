d5246c6349e23083f1ea7bcaba4804f3c11b9be7
MaciejZbierowski <maix19bep@gmail.com>
2017-03-05 02:18:44 +0100

    iplemented company's messages displaying

--- src/app/pages/company/company.ts
+++ src/app/pages/company/company.ts
@@ -17,3 +17,55 @@ export interface Company {
   createDate: Date;
   mainImageUrl: any;
 }
+
+export interface User {
+  id: number;
+  name: string;
+  status: any;
+  company: Company;
+  email: string;
+}
+
+export interface Company2 {
+  id: number;
+  name: string;
+  category: any;
+  createDate: Date;
+  mainImageUrl: any;
+}
+
+export interface NearestCompanyBranch {
+  id: number;
+  name: string;
+  city: string;
+  street: string;
+  streetNo: string;
+  latitude: number;
+  longitude: number;
+  description: string;
+  website: string;
+  phoneNumber: string;
+  email: string;
+  openingHours: string;
+  company: Company2;
+  createDate: Date;
+  distance: number;
+  main: boolean;
+}
+
+export interface ObjectList {
+  id: number;
+  content: string;
+  type: string;
+  startDate: Date;
+  endDate: Date;
+  createDate: Date;
+  status: string;
+  companyBranchList: any;
+  user: User;
+  location: any;
+  distance: number;
+  mainImageUrl: string;
+  companyBranchCount: number;
+  nearestCompanyBranch?: NearestCompanyBranch;
+}
