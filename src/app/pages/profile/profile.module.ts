5eb92d5e706697216dfef7b29af90504502e99dd
Wojciech Grazawski <wojciech.grazawski@wp.pl>
2017-03-06 23:51:31 +0100

    Personalizacja cz.2

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,13 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { FormsModule } from '@angular/forms';
 import { routing } from './profile.routing';
 import { MdTabsModule } from '@angular2-material/tabs';
+import { ImageUploadModule } from 'angular2-image-upload';
 
 @NgModule({
   imports: [
     CommonModule,
     MdTabsModule,
+    ImageUploadModule.forRoot(),
     routing
   ],
   declarations: [
